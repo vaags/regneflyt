fb02c95fadaf49696e635cb90a28fd77ddd133bb
Thomas Vaags <thomas.vaags@gmail.com>
2022-01-19 08:58:01 +0100

    Configure tailwind

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,1 @@
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<h1 class="text-3xl font-bold underline bg-gray-400">Hello world!</h1>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,19 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
+		colors: {
+			black: '#000',
+			white: '#fff',
+			gray: colors.stone,
+			orange: colors.orange,
+			red: colors.red,
+			green: colors.emerald,
+			blue: colors.sky,
+			yellow: colors.amber
+		},
 		extend: {}
 	},
-	plugins: []
+	plugins: [require('@tailwindcss/forms')]
 };
