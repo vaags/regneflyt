a1f68ec18be1ad40be6ca91d5db17fe49f15426a
Thomas Vaags <thomas.vaags@gmail.com>
2022-02-06 13:10:00 +0100

    Reduced logo size

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -6,7 +6,7 @@
 <div class="container mx-auto max-w-lg px-1 md:px-3 py-1 md:py-2">
 	<header>
 		<h1 class="text-right -mb-1">
-			<span class="font-handwriting text-3xl md:text-4xl text-orange-500">Regneflyt</span>
+			<span class="font-handwriting text-2xl md:text-3xl text-orange-500">Regneflyt</span>
 			<small class="font-light text-xs text-gray-400"
 				><a href="https://github.com/vaags/regneflyt">{version}</a></small
 			>
