b24669eacd35a2111020c7b99288cf54a49cba72
Thomas Vaags <thomas.vaags@gmail.com>
2022-02-11 10:04:50 +0100

    Hide version on load

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,15 +1,22 @@
 <script lang="ts">
 	import '../app.css'
 	const version = APP_VERSION
+
+	let showVersion: boolean
 </script>
 
 <div class="container mx-auto max-w-lg px-1 md:px-3 py-1 md:py-2">
 	<header>
 		<h1 class="text-right -mb-1">
-			<span class="font-handwriting text-2xl md:text-3xl text-orange-600">Regneflyt</span>
-			<small class="font-light text-xs text-gray-400"
-				><a href="https://github.com/vaags/regneflyt">{version}</a></small
+			<span
+				on:click={() => (showVersion = !showVersion)}
+				class="font-handwriting text-2xl md:text-3xl text-orange-600">Regneflyt</span
 			>
+			{#if showVersion}
+				<small class="font-light text-xs text-gray-400"
+					><a href="https://github.com/vaags/regneflyt">{version}</a></small
+				>
+			{/if}
 		</h1>
 	</header>
 	<main>
