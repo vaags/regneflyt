7b18e945158041cab22fcff47a78acb7e48b6d34
Thomas Vaags <thomas.vaags@gmail.com>
2022-02-19 20:31:50 +0100

    Implement share api

--- src/components/panels/SharePanel.svelte
+++ src/components/panels/SharePanel.svelte
@@ -2,25 +2,28 @@
 	import PanelComponent from '../widgets/PanelComponent.svelte'
 	import { slide } from 'svelte/transition'
 	import { onMount } from 'svelte'
-	import LabelComponent from '../widgets/LabelComponent.svelte'
 	import type { TransitionDuration } from '../../models/TransitionDuration'
+	import ButtonComponent from '../widgets/ButtonComponent.svelte'
 
 	export let transitionDuration: TransitionDuration
 
-	let textAreaDom: any
 	let titleDom: any
-	let shareLinkCopied: boolean
 	let shareTitle: string
 
-	function copyShareLinkToClipboard() {
-		textAreaDom.focus()
-		textAreaDom.select()
+	async function shareUrl() {
+		const shareData = {
+			title: `${shareTitle} - Regneflyt`,
+			text: 'Tren hoderegning med Regneflyt!',
+			url: `${window.location.protocol}//${window.location.host}${window.location.pathname}${
+				window.location.search
+			}&title=${encodeURIComponent(shareTitle)}&showSettings=false`
+		}
 
-		const successful = document.execCommand('copy')
-		if (!successful) {
-			console.error('unable to copy share link')
-		} else {
-			shareLinkCopied = true
+		try {
+			await navigator.share(shareData)
+			console.log('shared successfully')
+		} catch (err) {
+			console.log('Error: ' + err)
 		}
 	}
 
@@ -39,34 +42,18 @@
 
 <div transition:slide|local={transitionDuration} on:introend={() => scrollToBottom()} id="share">
 	<PanelComponent heading="Deling">
-		<label class="text-lg"
-			>Tittel
-			<input
-				type="text"
-				maxlength="50"
-				bind:this={titleDom}
-				on:keyup={() => (shareLinkCopied = false)}
-				class="rounded w-3/4 block text-lg"
-				bind:value={shareTitle}
-			/>
-		</label>
-		<label class="block mt-4">
-			<span class="text-lg">Lenke</span>
-			<span class="float-right">
-				<LabelComponent on:click={copyShareLinkToClipboard}>
-					{shareLinkCopied ? 'Kopiert!' : 'Trykk for å kopiere'}
-				</LabelComponent>
-			</span>
-			<textarea
-				class="rounded w-full font-mono text-xs"
-				rows="4"
-				bind:this={textAreaDom}
-				value="{window.location.protocol +
-					'//' +
-					window.location.host +
-					window.location.pathname +
-					window.location.search}&title={encodeURIComponent(shareTitle)}&showSettings=false"
-			/>
-		</label>
+		<div class="flex items-end">
+			<label class="text-lg"
+				>Tittel
+				<input
+					type="text"
+					maxlength="50"
+					bind:this={titleDom}
+					class="rounded block text-lg mr-1"
+					bind:value={shareTitle}
+				/>
+			</label>
+			<ButtonComponent on:click={() => shareUrl()}>Del</ButtonComponent>
+		</div>
 	</PanelComponent>
 </div>
