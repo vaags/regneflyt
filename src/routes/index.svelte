00e834daac9903c483908f951fbd9e0dfc9ac450
Thomas Vaags <thomas.vaags@gmail.com>
2022-02-24 12:23:34 +0100

    Removed appService

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,14 +8,13 @@
 	import { getQuizScoreSum } from '../services/scoreService'
 	import type { QuizScores } from '../models/QuizScores'
 	import { getQuiz } from '../services/quizService'
-	import { fakeInputFocus } from '../services/appService'
 	import { QuizState } from '../models/constants/QuizState'
 	import type { Quiz } from 'src/models/Quiz'
 
 	let quizScores: QuizScores
 	let puzzleSet: Puzzle[]
 	let quiz: Quiz
-	let fakeInput: any
+	let fakeInput: HTMLInputElement
 	let showContent: boolean
 
 	function getReady(event: any) {
@@ -55,6 +54,26 @@
 			left: 0,
 			behavior: 'smooth'
 		})
+	}
+
+	export function fakeInputFocus(fakeInput: HTMLInputElement) {
+		// Hack to get Safari / Ios to focus
+		// create invisible dummy input to receive the focus first
+		// Ref: https://stackoverflow.com/a/45703019
+		if (!fakeInput) {
+			fakeInput = document.createElement('input')
+			fakeInput.setAttribute('type', 'number')
+			fakeInput.style.position = 'absolute'
+			fakeInput.style.opacity = '0'
+			fakeInput.style.height = '0'
+			fakeInput.style.fontSize = '16px' // disable auto zoom
+
+			// you may need to append to another element depending on the browser's auto
+			// zoom/scroll behavior
+			document.body.prepend(fakeInput)
+		}
+
+		fakeInput.focus()
 	}
 
 	onMount(() => {
