638a69e705dac9ae69c5cea2ae6ae5643c543e19
Thomas Vaags <thomas.vaags@gmail.com>
2022-02-26 12:36:48 +0100

    Added github-link

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,20 +1,11 @@
 <script lang="ts">
 	import '../app.css'
-	const version = APP_VERSION
-
-	let showVersion: boolean
 </script>
 
 <div class="container mx-auto max-w-lg px-1 md:px-3 py-1 md:py-2">
 	<header>
 		<h1 class="text-right -mb-1">
-			<span
-				on:click={() => (showVersion = !showVersion)}
-				class="font-handwriting text-2xl md:text-3xl text-orange-600">Regneflyt</span
-			>
-			<small class="font-light text-xs text-gray-400 {!showVersion ? 'hidden' : ''}"
-				><a href="https://github.com/vaags/regneflyt">{version}</a></small
-			>
+			<span class="font-handwriting text-2xl md:text-3xl text-orange-600">Regneflyt</span>
 		</h1>
 	</header>
 	<main>
