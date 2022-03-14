983510d5916531ed9283748ceb9f8b50259512a5
Thomas Vaags <thomas.vaags@gmail.com>
2022-03-14 16:16:22 +0100

    Sorting css classes

--- src/components/icons/CheckmarkComponent.svelte
+++ src/components/icons/CheckmarkComponent.svelte
@@ -3,6 +3,6 @@
 </script>
 
 <span
-	class="block w-4.5 h-2.5 border-green-600 border-b-4 border-l-4 transform -rotate-45 mb-2 ml-0.5 mt-1"
+	class="mb-2 ml-0.5 mt-1 block h-2.5 w-4.5 -rotate-45 transform border-b-4 border-l-4 border-green-600"
 	aria-label={label}
 />
