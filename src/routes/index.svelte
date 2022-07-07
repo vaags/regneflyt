fc5fd9e518f03272202e3f4da32c5c1fb0d5c370
Thomas Vaags <thomas.vaags@inmeta.no>
2022-07-07 11:32:52 +0200

    Removed keyboard focus hack

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -27,7 +27,6 @@
 		quiz.state = QuizState.AboutToStart
 		showWelcomePanel = false
 		scrollToTop()
-		fakeInputFocus(fakeInput)
 	}
 
 	const startQuiz = () => (quiz.state = QuizState.Started)
@@ -54,26 +53,6 @@
 			left: 0,
 			behavior: 'smooth'
 		})
-	}
-
-	export function fakeInputFocus(fakeInput: HTMLInputElement) {
-		// Hack to get Safari / Ios to focus
-		// create invisible dummy input to receive the focus first
-		// Ref: https://stackoverflow.com/a/45703019
-		if (!fakeInput) {
-			fakeInput = document.createElement('input')
-			fakeInput.setAttribute('type', 'number')
-			fakeInput.style.position = 'absolute'
-			fakeInput.style.opacity = '0'
-			fakeInput.style.height = '0'
-			fakeInput.style.fontSize = '16px' // disable auto zoom
-
-			// you may need to append to another element depending on the browser's auto
-			// zoom/scroll behavior
-			document.body.prepend(fakeInput)
-		}
-
-		fakeInput.focus()
 	}
 
 	onMount(() => {
