00d797163990edc5d0b7ae315dedc7bb05268923
Thomas Vaags <thomas.vaags@inmeta.no>
2022-07-21 12:37:12 +0200

    Set touch action

--- src/components/widgets/NumpadComponent.svelte
+++ src/components/widgets/NumpadComponent.svelte
@@ -97,7 +97,7 @@
 	}
 </script>
 
-<div class="mx-auto w-7/12">
+<div class="mx-auto w-7/12 touch-none">
 	<div class="mb-1.5 grid grid-cols-3 gap-1.5 text-center text-gray-800 md:mb-2 md:gap-2">
 		{#each digits as i}
 			<NumpadButtonComponent on:click={() => onClick(i.toString())}>
