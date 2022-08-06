7365cd46420727db38611303803be6c52b96eea2
Thomas Vaags <thomas.vaags@inmeta.no>
2022-08-06 11:59:22 +0200

    Update packages

--- src/components/panels/WelcomePanel.svelte
+++ src/components/panels/WelcomePanel.svelte
@@ -3,7 +3,7 @@
 	import { AppSettings } from '../../models/constants/AppSettings'
 	import PanelComponent from '../widgets/PanelComponent.svelte'
 
-	const version = '3.11.0'
+	const version = '3.11.1'
 </script>
 
 <div transition:slide|local={AppSettings.transitionDuration}>
