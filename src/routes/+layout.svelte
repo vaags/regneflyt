de85ea17f1e58fdbc11cf7fd24519c2b7675bb62
Thomas Vaags <thomas.vaags@gmail.com>
2026-03-08 12:09:18 +0100

    Disable ssr

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
 	import '../app.css'
 	import { onMount } from 'svelte'
 	import * as m from '$lib/paraglide/messages.js'
-	import { getLocale, setLocale, locales } from '$lib/paraglide/runtime.js'
+	import { getLocale } from '$lib/paraglide/runtime.js'
 
 	function updateHead() {
 		document.documentElement.lang = getLocale()
@@ -12,19 +12,6 @@
 	}
 
 	onMount(() => {
-		const stored = document.cookie
-			.split('; ')
-			.find((c) => c.startsWith('PARAGLIDE_LOCALE='))
-			?.split('=')[1]
-
-		if (!stored) {
-			const browserLang = navigator.language.split('-')[0]
-			const match = locales.find((l) => l === browserLang)
-			if (match && match !== getLocale()) {
-				setLocale(match, { reload: false })
-			}
-		}
-
 		updateHead()
 	})
 </script>
