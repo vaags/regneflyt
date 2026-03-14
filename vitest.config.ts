6068006c7974c65c9f66f0555262d939f846e5cc
Thomas Vaags <thomas.vaags@gmail.com>
2026-03-14 04:40:21 +0100

    Vite 8 update

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 import path from 'path'
 
 export default defineConfig({
-	plugins: [svelte({ hot: false, compilerOptions: { hmr: false } })],
+	plugins: [svelte()],
 	resolve: {
 		alias: {
 			$lib: path.resolve(__dirname, './src/lib'),
